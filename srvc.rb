840e722c29fa0ea9abf52c0becc2b543489bea64
John Shaffer <john@insilica.co>
2022-08-05 08:45:02 -0500

    Version 0.2.0

--- srvc.rb
+++ srvc.rb
@@ -1,14 +1,14 @@
 class Srvc < Formula
-    version '0.1.0'
+    version '0.2.0'
     desc "Sysrev version control."
     homepage "https://github.com/insilica/rs-srvc"
 
     if OS.mac?
         url "https://github.com/insilica/rs-srvc/releases/download/v#{version}/srvc-v#{version}-x86_64-apple-darwin.tar.gz"
-        sha256 "39c2b1b19a55735bf332e673411154aba102a7d4924c49180d87a75465ebc600"
+        sha256 "6e93717d42b8b596100943e32b84b82eae00e7b9ced997b893fc2e9562decbbf"
     elsif OS.linux?
         url "https://github.com/insilica/rs-srvc/releases/download/v#{version}/srvc-v#{version}-x86_64-unknown-linux-musl.tar.gz"
-        sha256 "a63a06faa8d469115752cae116a18a9baaa3ee35e97015ef62b50b3e037ec331"
+        sha256 "776eedff81472daa228dfa20fb95d6235afdea66ade14b71846f6393056c29ea"
     end
 
     def install
